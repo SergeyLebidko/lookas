5aafd4981bffa1fa328957681bd8aa5f94ce8d6e
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-31 16:27:36 +0300

    Правки настроек линтера. Добавлен каркас для прелоадера

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Preloader from "../Preloader/Preloader";
+import "./App.scss";
+
+function App() {
+    return (
+        <div className="app">
+            <Preloader/>
+        </div>
+    );
+}
+
+export default App;
