ce4f0b504c5a8105058d0a5fb9e821fa39f67d20
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-31 19:30:34 +0300

    Завершен прелоадер

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useRef, useState} from "react";
+import classNames from "classnames";
 import "./Preloader.scss";
 
+const RISE_MODE = 'rm';
+const JUMP_MODE = 'jm';
+const FULL_MODE = 'fm';
+
 function Preloader() {
+    const [animation, setAnimation] = useState(RISE_MODE);
+    const jumpCount = useRef(0);
+
+    const pulsarClasses = classNames(
+        'preloader__pulsar',
+        {
+            'preloader__rised_pulsar': animation === RISE_MODE,
+            'preloader__jumped_pulsar': animation === JUMP_MODE,
+            'preloader__full_pulsar': animation === FULL_MODE
+        }
+    );
+
+    const animationSwitcher = () => {
+        if (animation === RISE_MODE) {
+            setAnimation(JUMP_MODE);
+            return;
+        }
+        if (animation === JUMP_MODE) jumpCount.current++;
+        if (jumpCount.current === 5) setAnimation(FULL_MODE);
+    }
+
     return (
         <div className="preloader">
-
+            <div className={pulsarClasses} onAnimationIteration={animationSwitcher} onAnimationEnd={animationSwitcher}/>
         </div>
     );
 }
