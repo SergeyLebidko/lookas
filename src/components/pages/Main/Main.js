fb71ac9b84292d676a7666fe0c22d8e6bfd37363
SergeyLebidko <it.sergeyler@mail.ru>
2021-11-01 17:13:23 +0300

    Создан первый вариант верстки для компонента Main

--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -9,10 +9,14 @@ function Main({imgLoadHandler}) {
     const {hasPreloader} = useContext(appContext);
 
     const coverClasses = classNames('main__cover', {'main__cover_rise': !hasPreloader});
+    const titleClasses = classNames('main__title', {'main__title_rise': !hasPreloader});
+    const descriptionClasses = classNames('main__description', {'main__description_rise': !hasPreloader});
 
     return (
         <main className="main">
             <img className={coverClasses} src={mainImg} onLoad={imgLoadHandler}/>
+            <h1 className={titleClasses}>Tanya Lookas</h1>
+            <h3 className={descriptionClasses}>design & decor</h3>
         </main>
     );
 }
