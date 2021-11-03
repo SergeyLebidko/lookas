165041b2877e797cb1bd9d5dee4847ac191b54b9
SergeyLebidko <it.sergeyler@mail.ru>
2021-11-03 17:49:41 +0300

    Из Command.js удален тестовый код

--- src/components/pages/Command/Command.js
+++ src/components/pages/Command/Command.js
@@ -1,18 +1,13 @@
-import React, {useContext, useRef} from "react";
-import {useElementVisible} from "../../../utils/hooks";
+import React, {useContext} from "react";
 import {appContext} from "../../../utils/context";
 import "./Command.scss";
 
 function Command() {
-    const {hasPreloader, line, direction} = useContext(appContext);
-
-    const commandRef = useRef(null);
-    useElementVisible(commandRef, 'Command', line, direction);
-
+    const {hasPreloader} = useContext(appContext);
     if (hasPreloader) return null;
 
     return (
-        <section className="command bordered" ref={commandRef}>
+        <section className="command bordered">
             <h1>Вокруг меня коллектив профессионалов</h1>
             <p>
                 Я работаю в команде с флористами, фотографами, ведущими, осветителями,
