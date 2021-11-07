2a4f87f1253de2174b41bdd1920860135824232d
SergeyLebidko <it.sergeyler@mail.ru>
2021-11-07 14:11:01 +0300

    Глобальный рефакторинг стилей

--- src/components/common/AboutMyself/AboutMyself.js
+++ src/components/common/AboutMyself/AboutMyself.js
@@ -8,8 +8,22 @@ function AboutMyself() {
             <h2 className="about_myself__small_title">Я декоратор-реставратор с десятилетним опытом работы.</h2>
             <h2 className="about_myself__small_title">Руковожу студией декора &quot;Белый лён&quot;</h2>
             <div className="about_myself__links">
-                <a href="https://vk.com/whitelinen_snk">Vk</a>
-                <a href="https://www.instagram.com/tanya_lookas/">Instagram</a>
+                <a
+                    className="about_myself__link"
+                    href="https://vk.com/whitelinen_snk"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Vk
+                </a>
+                <a
+                    className="about_myself__link"
+                    href="https://www.instagram.com/tanya_lookas/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Instagram
+                </a>
             </div>
         </div>
     );
