8ef050e169c84a87aaf960dc40e23d88058a0c5f
SergeyLebidko <it.sergeyler@mail.ru>
2021-11-07 14:26:05 +0300

    Добавлена анимация появления аватара и лого

--- src/components/common/Avatar/Avatar.js
+++ src/components/common/Avatar/Avatar.js
@@ -1,16 +1,25 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
+import classNames from "classnames";
 import avatar from "../../../content/images/avatar.jpg";
 import wl from "../../../content/images/wl.jpg";
 import {appContext} from "../../../utils/context";
 import "./Avatar.scss";
 
 function Avatar() {
-    const {imgLoadHandler} = useContext(appContext);
+    const [hasVisible, setHasVisible] = useState(false);
+    const {line, imgLoadHandler} = useContext(appContext);
+
+    useEffect(() => {
+        if (line > 400) setHasVisible(true);
+    }, [line]);
+
+    const avatarClasses = classNames('avatar__photo', {'avatar__photo_rised': hasVisible});
+    const logoClasses = classNames('avatar__logo', {'avatar__logo_rised': hasVisible});
 
     return (
         <div className="avatar">
-            <img className="avatar__photo" src={avatar} onLoad={imgLoadHandler}/>
-            <img className="avatar__logo" src={wl} onLoad={imgLoadHandler}/>
+            <img className={avatarClasses} src={avatar} onLoad={imgLoadHandler}/>
+            <img className={logoClasses} src={wl} onLoad={imgLoadHandler}/>
         </div>
     );
 }
