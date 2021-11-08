fcf3f2fcc94900e9a8bbeb6afce3c470e1d9638d
SergeyLebidko <it.sergeyler@mail.ru>
2021-11-08 13:53:47 +0300

    Добавлены анимации появления текста в AboutMyself.scss

--- src/components/common/AboutMyself/AboutMyself.js
+++ src/components/common/AboutMyself/AboutMyself.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useContext, useRef, useState} from "react";
+import classNames from "classnames";
+import {useVisibleControl} from "../../../utils/hooks";
+import {appContext} from "../../../utils/context";
 import "./AboutMyself.scss";
 
 function AboutMyself() {
+    const [hasVisible, setHasVisible] = useState(false);
+    const {line, direction} = useContext(appContext);
+    const elementRef = useRef(null);
+
+    useVisibleControl(elementRef, line, direction, 1 / 2, setHasVisible, hasVisible);
+
+    const bigTitleClasses = classNames('about_myself__big_title', {'about_myself__big_title_rise': hasVisible});
+    const smallTitleClasses = classNames('about_myself__small_title', {'about_myself__small_title_rise': hasVisible});
+    const descriptionClasses = classNames('about_myself__description', {'about_myself__description_rise': hasVisible});
+    const linksClasses = classNames('about_myself__links', {'about_myself__links_rise': hasVisible});
+
     return (
-        <div className="about_myself">
-            <h1 className="about_myself__big_title">Всем привет, я Tanya Lookas!</h1>
-            <h2 className="about_myself__small_title">Я декоратор-реставратор с десятилетним опытом работы.</h2>
-            <h2 className="about_myself__small_title">Руковожу студией декора &quot;Белый лён&quot;</h2>
-            <div className="about_myself__description">
+        <div className="about_myself" ref={elementRef}>
+            <h1 className={bigTitleClasses}>Всем привет, я Tanya Lookas!</h1>
+            <h2 className={smallTitleClasses}>Я декоратор-реставратор с десятилетним опытом работы.</h2>
+            <h2 className={smallTitleClasses}>Руковожу студией декора &quot;Белый лён&quot;</h2>
+            <div className={descriptionClasses}>
                 <p>
                     Студия декора Белый лён - это коллектив творческий людей, готовых делиться вдохновением,
                     подсказывать и помогать в оформлении и организации торжеств!
@@ -21,7 +35,7 @@ function AboutMyself() {
                     всех творческих людей к сотрудничеству!
                 </p>
             </div>
-            <div className="about_myself__links">
+            <div className={linksClasses}>
                 <a
                     className="about_myself__link"
                     href="https://vk.com/whitelinen_snk"
